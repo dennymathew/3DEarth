fddb073a07237db00e3edfb3d467532029c69035
Denny Mathew <denny.mathew@cabotsolutions.com>
2017-12-04 11:26:57 +0530

    Initial Commit

---
+++ 3DEarth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DEarth
+//
+//  Created by Denny Mathew on 04/12/17.
+//  Copyright © 2017 Denny Mathew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
